6e0d2497d0a4c564788588659d62ed956f9a4d1e
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2025-10-08 10:05:22 +0100

    making changes to the header, footer, contact

--- assets/footer.php
+++ assets/footer.php
@@ -1,5 +1,5 @@
 
-<body>
+
 <footer class="bg-white dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
@@ -85,3 +85,4 @@
     </div>
 </footer>
 </body>
+</html>
