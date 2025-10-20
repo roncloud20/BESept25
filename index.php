a4d7eb436f11165281ed9ed627b13276b58a91b7
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2025-10-20 12:31:05 +0100

    Upload a file

--- index.php
+++ index.php
@@ -83,21 +83,52 @@ require_once('assets/header.php');
 // echo "</pre>";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $entry = $_POST['entry'];
-    // if(preg_match('/(0[789][01]\d{8}$)|(^\+234[789][01]\d{8}$)/', $entry)){
-    if(preg_match('/^0|^\+234[789][01]\d{8}$/', $entry)){
-        echo "<h1>$entry is valid</h1>";
+    $image = $_FILES['imageUpload'];
+    // echo "<pre>";
+    // var_dump($image);
+    // echo "</pre>";
+    if($image['type'] === 'image/jpeg' || $image['type'] === 'image/jpg' || $image['type'] === 'image/png') {
+        echo $image['name'];
     } else {
-        echo "<h1>Your number: $entry does not exist</h1>";
-    }
+        die("File type is not supported");
+    };
+    // if(preg_match('/(0[789][01]\d{8}$)|(^\+234[789][01]\d{8}$)/', $entry)){
+    // if (preg_match('/^0|^\+234[789][01]\d{8}$/', $entry)) {
+    //     echo "<h1>$entry is valid</h1>";
+    // } else {
+    //     echo "<h1>Your number: $entry does not exist</h1>";
+    // }
 }
 
 ?>
 <h1 class="text-3xl font-bold">Hello World</h1>
-<form action="" method="post">
+<form action="" method="post" enctype="multipart/form-data">
     <!-- <input type="text" name="person" /> -->
+    <div class="col-span-full">
+        <label for="photo" class="block text-sm/6 font-medium text-gray-900">Photo</label>
+        <div class="mt-2 flex items-center gap-x-3">
+            <img class="w-15 h-15 rounded-full" src="assets/raystore.png" alt="Rounded avatar" id="display">
+            <input type="file" class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs inset-ring inset-ring-gray-300 hover:bg-gray-50" id="entry" name="imageUpload"/>
+        </div>
+    </div>
     <input type="text" name="entry" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+
     <input type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" />
 </form>
+
+<script>
+    const entry = document.querySelector('#entry');
+    const display = document.querySelector('#display');
+    entry.addEventListener('change', (e)=> {
+        let file = e.target.files[0];
+        if(file.type == "image/jpeg" || file.type == "image/jpg" || file.type == "image/png") {
+            display.src = URL.createObjectURL(file);
+            console.log(file);
+        } else {
+            alert('Please upload an image');
+        }
+    });
+</script>
 <?php
 // require_once('assets/footer.php');
 ?>
