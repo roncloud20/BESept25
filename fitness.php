71fdcae881bfa84802046dac8986ec9970a6cee1
Odunuga Mayowa <odunugamayowa5@gmail.com>
2025-10-20 09:43:38 +0100

    improving the fitness page

--- fitness.php
+++ fitness.php
@@ -93,7 +93,7 @@ if(preg_match('/^\+234|^0[789][01]\d{8}$/', $phone)){
             
         <div>
             <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone number</label>
-            <input type="tel" name="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678"  />
+            <input type="number" name="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678"  />
         </div>
         
         <div class="mb-6">
